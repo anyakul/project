f4b8888a4e1db7e6067f87223e1dc5a197793f64
anyakulikova111@ <anyakulikova111@gmail.com>
2023-05-23 11:04:30 +0300

    task1

--- python/prog003_Calculator/spy_log.py
+++
@@ -1,6 +0,0 @@
-import telebot
-import logging
-logger = telebot.logger
-
-logger.basicConfig(filename='log.txt', level=logging.DEBUG,
-    format=' %(asctime)s - %(levelname)s - %(message)s')
--- python/task023_MultNums/func.py
+++
@@ -1,5 +0,0 @@
-from random import randint
-
-
-def get_random(min_num, max_num, count):
-    return [randint(min_num, max_num) for i in range(0, count)]
